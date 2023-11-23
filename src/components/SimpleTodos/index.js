ee946ca794c5014b201ba9f234d0b1916a1ac8f6
mouli shankar <moulishankar3669@gmail.com>
2023-11-23 05:15:37 +0000

    new features added

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -8,40 +8,49 @@ const initialTodosList = [
   {
     id: 1,
     title: 'Book the ticket for today evening',
+    isChecked: false,
   },
   {
     id: 2,
     title: 'Rent the movie for tomorrow movie night',
+    isChecked: false,
   },
   {
     id: 3,
     title: 'Confirm the slot for the yoga session tomorrow morning',
+    isChecked: false,
   },
   {
     id: 4,
     title: 'Drop the parcel at Bloomingdale',
+    isChecked: false,
   },
   {
     id: 5,
     title: 'Order fruits on Big Basket',
+    isChecked: false,
   },
   {
     id: 6,
     title: 'Fix the production issue',
+    isChecked: false,
   },
   {
     id: 7,
     title: 'Confirm my slot for Saturday Night',
+    isChecked: false,
   },
   {
     id: 8,
     title: 'Get essentials for Sunday car wash',
+    isChecked: false,
   },
 ]
 
 class SimpleTodos extends Component {
   state = {
     todosList: initialTodosList,
+    todoTitle: '',
   }
 
   deleteTodo = id => {
@@ -53,19 +62,87 @@ class SimpleTodos extends Component {
     })
   }
 
-  render() {
+  isChecked = (id, value) => {
     const {todosList} = this.state
+    this.setState({
+      todosList: todosList.map(eachTodo => {
+        if (eachTodo.id === id) {
+          return {
+            ...eachTodo,
+            isChecked: value,
+          }
+        }
+        return eachTodo
+      }),
+    })
+  }
+
+  addTodo = () => {
+    const {todoTitle} = this.state
+
+    if (todoTitle.length > 0) {
+      this.setState(prevState => ({
+        todosList: [
+          ...prevState.todosList,
+          {
+            id:
+              prevState.todosList.length > 0
+                ? prevState.todosList[prevState.todosList.length - 1].id + 1
+                : 0,
+            title: todoTitle,
+            isChecked: false,
+          },
+        ],
+        todoTitle: '',
+      }))
+    }
+  }
+
+  changeTodoTitle = (id, value) => {
+    const {todosList} = this.state
+    this.setState({
+      todosList: todosList.map(eachTodo => {
+        if (eachTodo.id === id) {
+          return {
+            ...eachTodo,
+            title: value,
+          }
+        }
+        return eachTodo
+      }),
+    })
+  }
+
+  addNewTodoTitle = event => {
+    this.setState({todoTitle: event.target.value})
+  }
+
+  render() {
+    const {todosList, todoTitle} = this.state
 
     return (
       <div className="app-container">
         <div className="simple-todos-container">
           <h1 className="heading">Simple Todos</h1>
+          <div className="add-new-todo-container">
+            <input
+              onChange={this.addNewTodoTitle}
+              type="text"
+              value={todoTitle || ''}
+              placeholder="Add new todo"
+            />
+            <button onClick={this.addTodo} type="button">
+              ADD
+            </button>
+          </div>
           <ul className="todos-list">
             {todosList.map(eachTodo => (
               <TodoItem
                 key={eachTodo.id}
                 todoDetails={eachTodo}
                 deleteTodo={this.deleteTodo}
+                isCheckedTodo={this.isChecked}
+                changeTodoTitle={this.changeTodoTitle}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,19 +1,61 @@
+import {useState} from 'react'
+
 import './index.css'
 
 const TodoItem = props => {
-  const {todoDetails, deleteTodo} = props
-  const {id, title} = todoDetails
+  const {todoDetails, deleteTodo, isCheckedTodo, changeTodoTitle} = props
+  const {id, title, isChecked} = todoDetails
+  const [isSaved, setSaved] = useState(true)
+  const [editedTitle, editTitle] = useState(title)
 
   const onDeleteTodo = () => {
     deleteTodo(id)
   }
 
+  const changeTodoCheckbox = event => {
+    isCheckedTodo(id, event.target.checked)
+  }
+
+  const editAndSaved = () => {
+    setSaved(!isSaved)
+    if (!isSaved) {
+      changeTodoTitle(id, editedTitle)
+    }
+  }
+
+  const editTodoTitle = event => {
+    editTitle(event.target.value)
+  }
+
   return (
-    <li className="todo-item">
-      <p className="title">{title}</p>
-      <button type="button" className="delete-btn" onClick={onDeleteTodo}>
-        Delete
-      </button>
+    <li id={id} className="todo-item">
+      <div className="checkbox-title-container">
+        <input
+          onChange={changeTodoCheckbox}
+          className="checkbox"
+          type="checkbox"
+        />
+        {isSaved ? (
+          <p className={`title ${isChecked ? 'checked-todo' : null}`}>
+            {title}
+          </p>
+        ) : (
+          <input
+            className="edit-input"
+            onChange={editTodoTitle}
+            type="text"
+            value={editedTitle}
+          />
+        )}
+      </div>
+      <div className="edit-and-delete-buttons">
+        <button onClick={editAndSaved} className="edit-btn" type="button">
+          {isSaved ? 'Edit' : 'Save'}
+        </button>
+        <button type="button" className="delete-btn" onClick={onDeleteTodo}>
+          Delete
+        </button>
+      </div>
     </li>
   )
 }
